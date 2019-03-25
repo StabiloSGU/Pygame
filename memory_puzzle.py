677d4de27399f533e8b672b753d895a68b1e0e6f
Stb <stabilo1992@gmail.com>
2019-03-25 16:55:48 +0300

    debugging time

--- memory_puzzle.py
+++ memory_puzzle.py
@@ -1,7 +1,7 @@
 import random, pygame, sys
 from pygame.locals import *
 
-#options block
+# options block
 
 FPS = 30
 WINDOWWIDTH = 640
@@ -12,13 +12,13 @@ GAPSIZE = 10
 BOARDWIDTH = 10
 BOARDHEIGHT = 7
 
-#checkout assert
+# checkout assert
 assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'not even amount of boxes'
 
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH *(BOXSIZE + GAPSIZE))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT *(BOXSIZE + GAPSIZE))) / 2)
 
-#colors
+# colors
 GRAY = (100, 100, 100)
 NAVYBLUE = (60, 60, 100)
 WHITE = (255, 255, 255)
@@ -30,21 +30,43 @@ ORANGE = (255, 128, 0)
 PURPLE = (255, 0, 255)
 CYAN = (0, 255, 255)
 
-#color assignments
+# color assignments
 BGCOLOR = NAVYBLUE
 LIGHTBGCOLOR = GRAY
 BOXCOLOR = WHITE
 HIGHLIGHTCOLOR = BLUE
 
-#shapes definitions
+# shapes definitions
 DONUT = 'donut'
 SQUARE = 'square'
 DIAMOND = 'diamond'
 LINES = 'lines'
 OVAL = 'oval'
 
-#color and shape tuples
+# color and shape tuples
 ALLCOLORS = (RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN)
 ALLSHAPES = (DONUT, SQUARE, DIAMOND, LINES, OVAL)
-assert len(ALLCOLORS) * len(ALLSHAPES) * 2 >= BOARDWIDTH * BOARDHEIGHT, "board is too big, not enough shapes and or colors"
+assert len(ALLCOLORS) * len(ALLSHAPES) * 2 >= BOARDWIDTH * BOARDHEIGHT,\
+    "board is too big, not enough shapes and or colors"
 
+
+# main function
+def main():
+    global FPSCLOCK, DISPLAYSURF
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+
+    # main loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+
+if __name__ == '__main__':
+    main()
